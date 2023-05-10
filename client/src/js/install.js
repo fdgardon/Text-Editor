46381e9dabb0bdbf3cf968712308ebef19c40d51
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-05-09 19:34:18 -0700

    bug fixed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,6 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    console.log(butInstall);
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
